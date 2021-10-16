60323b6b71bb9b739552542cbbc6c9ea79999faa
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-10-16 13:19:01 +0200

    add ContactItem

---
+++ src/ContactItem/ContactItem.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import { useDeleteContactMutation } from '../redux/contact-app/contactsSlice';
+import Loader from 'react-loader-spinner';
+import toast from 'react-hot-toast';
+import css from './ContactItem.module.css';
+
+const ContactItem = ({ id, name, number }) => {
+  const [deleteContact, { isLoading: isDeleting }] = useDeleteContactMutation();
+
+  const contactDelete = (contactId, name) => {
+    deleteContact(contactId);
+    toast.success(`${name} delete with Contacts!`);
+  };
+
+  return (
+    <li key={id} className={css.contactsItem}>
+      <span>{name}:</span>
+      <span>{number}</span>
+      <span>
+        <button
+          onClick={() => contactDelete(id, name)}
+          className={css.listButton}
+          type="button"
+        >
+          {isDeleting ? (
+            <Loader type="Rings" color="#00BFFF" height={20} width={20} />
+          ) : (
+            'delete'
+          )}
+        </button>
+      </span>
+    </li>
+  );
+};
+
+export default ContactItem;
+
+ContactItem.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  number: PropTypes.string,
+};
--- src/ContactList/ContactList.jsx
+++ src/ContactList/ContactList.jsx
@@ -1,37 +1,29 @@
 import PropTypes from 'prop-types';
-import Loader from 'react-loader-spinner';
-import toast from 'react-hot-toast';
-import { useDeleteContactMutation } from '../redux/contact-app/contactsSlice';
+import ContactItem from '../ContactItem/ContactItem.jsx';
 import css from './ContactList.module.css';
 
 const ContactList = ({ contacts }) => {
-  const [deleteContact, { isLoading: isDeleting }] = useDeleteContactMutation();
-
-  const contactDelete = (contactId, name) => {
-    deleteContact(contactId);
-    toast.success(`${name} delete with Contacts!`);
-  };
-
   return (
     <ul className={css.contactsList}>
-      {contacts.map(({ id, name, number }) => (
-        <li key={id} className={css.contactsItem}>
-          <span>{name}:</span>
-          <span>{number}</span>
-          <span>
-            <button
-              disabled={isDeleting}
-              onClick={() => contactDelete(id, name)}
-              className={css.listButton}
-              type="button"
-            >
-              {isDeleting && (
-                <Loader type="Rings" color="#00BFFF" height={20} width={20} />
-              )}
-              delete
-            </button>
-          </span>
-        </li>
+      {contacts.map(contact => (
+        <ContactItem key={contact.id} {...contact} />
+        // <li key={id} className={css.contactsItem}>
+        //   <span>{name}:</span>
+        //   <span>{number}</span>
+        //   <span>
+        //     <button
+        //       disabled={isDeleting}
+        //       onClick={() => contactDelete(id, name)}
+        //       className={css.listButton}
+        //       type="button"
+        //     >
+        //       {isDeleting && (
+        //         <Loader type="Rings" color="#00BFFF" height={20} width={20} />
+        //       )}
+        //       delete
+        //     </button>
+        //   </span>
+        // </li>
       ))}
     </ul>
   );
